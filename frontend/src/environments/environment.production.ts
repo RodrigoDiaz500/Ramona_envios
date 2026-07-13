6b47b66592bbb9e4a2a0e9229fd751f1891f03fa
Rodrigo Diaz <81325661+RodrigoDiaz500@users.noreply.github.com>
2026-07-13 19:33:36 -0400

    Dockeriza frontend y microservicios para despliegue

---
+++ frontend/src/environments/environment.production.ts
@@ -0,0 +1,24 @@
+﻿const appOrigin = window.location.origin;
+
+export const environment = {
+  production: true,
+
+  apiUrl: '/api',
+
+  azure: {
+    authority:
+      'https://login.microsoftonline.com/common',
+
+    clientId:
+      'b82da08a-15ea-4bd6-902e-236d2d2e523a',
+
+    redirectUri:
+      appOrigin,
+
+    postLogoutRedirectUri:
+      `${appOrigin}/login`,
+
+    scope:
+      'api://b82da08a-15ea-4bd6-902e-236d2d2e523a/Acceso.Total'
+  }
+};
